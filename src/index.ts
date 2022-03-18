f40f36bf2062ea2661ec8cd9dd81405dbb780bbf
alexboscolo <51131658+alexboscolo@users.noreply.github.com>
2022-03-18 18:01:58 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as express from 'express';
+
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  return app.init();
+};
+
+createNestServer(server)
+
+    // tslint:disable-next-line:no-console
+    .then(v => console.log('Nest Ready'))
+    // tslint:disable-next-line:no-console
+    .catch(err => console.error('Nest broken', err));
+
+export const api = server;
